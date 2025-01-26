659d53d045181c4ec94c5a7ce4f7bf097c599ee4
ShaswatSrivastava <shaswat24678@gmail.com>
2025-01-26 13:13:42 +0530

    Data types

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "shaswat@google.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21345"
+accountCity = "Varanasi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
