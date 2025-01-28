f604242adf754f4d1c162273faff4427ea64d7c1
ShaswatSrivastava <shaswat24678@gmail.com>
2025-01-28 23:19:02 +0530

    Datatype summary

---
+++ datatypes summary.js
@@ -0,0 +1,20 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+
+//Arrays
+
+const heroes = ['Shaktiman','hero']
+
+//Objects
+let myObj = {
+    name: "Shaswat",
+    age: 19,
+}
+
+//Functions
+const myFunction = function() {
+    console.log("Hello World")
+    }
